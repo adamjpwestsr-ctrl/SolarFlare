cf93ca1ed4080cba5a4eabf7a7f972394ae78066
Adam West <adamjpwestsr@gmail.com>
2026-08-21 15:59:24 -0400

    v0.0.9 - Continued visual aesthetic improvements

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
+
+export default nextConfig;
+
 export default {
   experimental: {
     optimizeCss: false

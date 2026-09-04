31dee85d6bd86de8a9790ff1b4d46b06e198b89d
Adam West <adamjpwestsr@gmail.com>
2026-09-04 14:36:04 -0400

    v1.0.0 - Astronaut Explorer Game Addition

---
+++ app/game/layout.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function GameLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full h-full">
+      {children}
+    </div>
+  );
+}
